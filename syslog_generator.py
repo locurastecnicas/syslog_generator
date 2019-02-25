ce21f82ef50de73b6a357b10a3a3544b8e851af7
Eduardo Izaguirre Pazos <eizaguirrep@gmail.com>
2019-02-25 16:21:52 +0100

    Primera version sin tipos ni threading.

--- syslog_generator.py
+++ syslog_generator.py
@@ -4,12 +4,44 @@ import time
 import sys
 import syslog
 import signal
+import uuid
+
+## Variables globales.
+LOG_CONSTANTS={
+  "LOG_KERN": syslog.LOG_KERN,
+  "LOG_USER": syslog.LOG_USER,
+  "LOG_MAIL": syslog.LOG_MAIL,
+  "LOG_DAEMON": syslog.LOG_DAEMON,
+  "LOG_AUTH": syslog.LOG_AUTH,
+  "LOG_LPR": syslog.LOG_LPR,
+  "LOG_NEWS": syslog.LOG_NEWS,
+  "LOG_UUCP": syslog.LOG_UUCP,
+  "LOG_CRON": syslog.LOG_CRON,
+  "LOG_SYSLOG": syslog.LOG_SYSLOG,
+  "LOG_LOCAL0": syslog.LOG_LOCAL0,
+  "LOG_LOCAL1": syslog.LOG_LOCAL1,
+  "LOG_LOCAL2": syslog.LOG_LOCAL2,
+  "LOG_LOCAL3": syslog.LOG_LOCAL3,
+  "LOG_LOCAL4": syslog.LOG_LOCAL4,
+  "LOG_LOCAL5": syslog.LOG_LOCAL5,
+  "LOG_LOCAL6": syslog.LOG_LOCAL6,
+  "LOG_LOCAL7": syslog.LOG_LOCAL7,
+  "LOG_EMERG": syslog.LOG_EMERG,
+  "LOG_ALERT": syslog.LOG_ALERT,
+  "LOG_CRIT": syslog.LOG_CRIT,
+  "LOG_ERR": syslog.LOG_ERR,
+  "LOG_WARNING": syslog.LOG_WARNING,
+  "LOG_NOTICE": syslog.LOG_NOTICE,
+  "LOG_INFO": syslog.LOG_INFO,
+  "LOG_DEBUG": syslog.LOG_DEBUG
+}
 
 class syslog_entry:
-  def __init__(self, secsdelay, logfacility, logpriority):
+  def __init__(self, secsdelay, logfacility, logpriority, entry_uuid):
     self.delay=secsdelay
     self.facility=logfacility
     self.priority=logpriority
+    self.uuid=entry_uuid
 
   def printConfig(self):
     print("Using the following configuration:")
@@ -23,15 +55,16 @@ def control_signal(signal_control,signal_handler):
   sys.exit()
 
 def parseArgs(arguments):
-  if (" ".join(arguments)).lower().find("help") != -1 or 1<len(arguments)<3:
+  if (" ".join(arguments)).lower().find("help") != -1 or 1<=len(arguments)<3:
     print("")
+    print(" -----------------------")
     print(" syslog_generator - v1.0")
     print(" -----------------------")
     print(" Generate syslog entries of different types and information.")
     print(" If invoked without arguments the default configuration assumed is:")
     print(" delay = 1 sec.")
-    print(" facility = LOG_INFO")
-    print(" priority = LOG_DAEMON")
+    print(" facility = LOG_DAEMON")
+    print(" priority = LOG_INFO")
     print("============================")
     print(" At least three arguments are required:")
     print("  --delay=N => Number of seconds between entries.")
@@ -40,40 +73,45 @@ def parseArgs(arguments):
     print("  --help Print this screen")
     sys.exit(0)
 
-  if len(arguments) == 1:
+  if len(arguments) == 0:
     config={
       "delay": 1,
-      "facility": syslog.LOG_INFO,
-      "priority": syslog.LOG_DAEMON
+      "facility": syslog.LOG_DAEMON,
+      "priority": syslog.LOG_INFO
     } 
     print("Using default configuration:")
     print(" delay = 1 sec.")
-    print(" facility = LOG_INFO")
-    print(" priority = LOG_DAEMON")
+    print(" facility = LOG_DAEMON")
+    print(" priority = LOG_INFO")
     print("============================")
-
-  for argsStr in arguments:
-    print("Parametro "+ argsStr)
+  else:
+    config={}
+    for argumentStr in arguments:
+      tempStr=argumentStr.lower().strip("--")
+      tempTuple=tempStr.partition("=")
+      if tempTuple[0] == "delay":
+        config[tempTuple[0]]=tempTuple[2]
+      else:
+        config[tempTuple[0]]=LOG_CONSTANTS[tempTuple[2].upper()]
 
   return(config)
 
 def main():
 
-  configValues={}
   signal.signal(signal.SIGINT, control_signal)
-  configValues=parseArgs(sys.argv)
+  configValues=parseArgs(sys.argv[1:(len(sys.argv))])
 
-  logaction=syslog_entry(configValues["delay"],configValues["facility"],configValues["priority"])
+  logaction=syslog_entry(configValues["delay"],configValues["facility"],configValues["priority"],uuid.uuid4())
 
   logaction.printConfig()
- 
+
   syslog.openlog(logoption=syslog.LOG_PID,facility=logaction.facility)
   while 1:
-    time.sleep(logaction.delay)
+    time.sleep(int(logaction.delay))
     infodate=time.ctime(time.time())
-    syslogline="%s - Linea de syslog"
-    print(syslogline%infodate)
-    syslog.syslog(logaction.priority,syslogline%infodate)
+    syslogline="%s - Linea de syslog %s"
+    print(syslogline%(infodate,logaction.uuid))
+    syslog.syslog(logaction.priority,syslogline%(infodate,logaction.uuid))
 
 if __name__ == '__main__':
   main()
